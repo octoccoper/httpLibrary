bd6fa82acd42fba06cb70bea9952477b16a599ad
octoccoper <octocopper@gmail.com>
2019-01-07 17:53:28 +0200

    added function for post request

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const http = new easyHTTP;
 
 // Get Posts
-http.get("https://jsonplaceholder.typicode.com/posts", function(error, response) {
+// http.get("https://jsonplaceholder.typicode.com/posts", function(error, response) {
+//   if(error) {
+//     console.log(error);
+//   } else {
+//     console.log(response);
+//   }
+
+// });
+
+// Create data
+const data = {
+  title: "Super-puper new titla!!",
+  body: "This is a content for custom created data."
+}
+
+// Create post
+http.post("https://jsonplaceholder.typicode.com/posts", data, function(error, response) {
   if(error) {
     console.log(error);
   } else {
     console.log(response);
   }
-
 });
-
--- easyhttp.js
+++ easyhttp.js
@@ -2,6 +2,7 @@ function easyHTTP() {
   this.http = new XMLHttpRequest();
 }
 
+// Make an HTTP GET request
 easyHTTP.prototype.get = function (url, callback) {
   this.http.open("GET", url, true);
 
@@ -16,4 +17,20 @@ easyHTTP.prototype.get = function (url, callback) {
   };
 
   this.http.send();
+}
+
+// Make an HTTP POST request
+easyHTTP.prototype.post = function (url, data, callback) {
+  this.http.open("POST", url, true);
+  this.http.setRequestHeader("Content-type", "application/json");
+
+  let self = this;
+
+  this.http.onload = function () {
+      callback(null, self.http.responseText);
+  };
+
+  this.http.send(JSON.stringify(data));
+
+
 }
